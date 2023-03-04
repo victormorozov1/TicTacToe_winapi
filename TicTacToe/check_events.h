6a059d5c0cd8679e268c9647a5dc37a01941f1a6
victormorozov1 <vismo@bk.ru>
2023-03-04 19:53:00 +0500

    rewrite case of if in check_events

--- TicTacToe/check_events.h
+++ TicTacToe/check_events.h
@@ -17,79 +17,75 @@ void process_click(HWND hWnd, LPARAM lParam, Game* game, char symbol) {
 }
 
 LRESULT CALLBACK check_events(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam, Game* game, UINT synchMessage) {
-    switch(msg) {
-        case WM_SIZE: {
-            InvalidateRect(hWnd, NULL, TRUE);
-            return 0;
-        }
-        case WM_DESTROY: {
-            PostQuitMessage(0);
-            return 0;
-        }
-        case WM_PAINT: {
-            //game->draw();
-            game->count_sz();
-            //game->painter.draw_line(0, 0, 200, 200, 4, Color{100, 100, 100});
+    if (msg ==  WM_SIZE) {
+        InvalidateRect(hWnd, NULL, TRUE);
+        return 0;
+    }
+    else if (msg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    else if (msg == WM_PAINT) {
+        //game->draw();
+        game->count_sz();
+        //game->painter.draw_line(0, 0, 200, 200, 4, Color{100, 100, 100});
             
-            return 0;
+        return 0;
+    }
+    else if (msg == WM_MOUSEMOVE) {
+        return 0;
+    }
+    else if (msg == WM_LBUTTONUP) {
+        process_click(hWnd, lParam, game, '0');
+        InvalidateRect(hWnd, NULL, TRUE);
+        PostMessage(HWND_BROADCAST, synchMessage, NULL, NULL);
+        return 0;
+    }
+    else if (msg == WM_RBUTTONUP) {
+        process_click(hWnd, lParam, game, 'x');
+        InvalidateRect(hWnd, NULL, TRUE);
+        PostMessage(HWND_BROADCAST, synchMessage, NULL, NULL);
+        return 0;
+    }
+    else if (msg == WM_MOUSEWHEEL)
+    {
+        if ((int16_t)HIWORD(wParam) > 0) {
+            game->painter.gridColor += 1;
         }
-        case WM_MOUSEMOVE: {
-            return 0;
+        else {
+            game->painter.gridColor -= 1;
         }
-        case WM_LBUTTONUP: {
-            process_click(hWnd, lParam, game, '0');
-            InvalidateRect(hWnd, NULL, TRUE);
-            PostMessage(HWND_BROADCAST, synchMessage, NULL, NULL);
-            return 0;
+        //game->draw();
+        PostMessage(HWND_BROADCAST, synchMessage, NULL, NULL);
+        InvalidateRect(hWnd, NULL, TRUE);
+        return 0;
+    }
+    else if (msg == WM_KEYDOWN) {
+        if (((GetKeyState(VK_CONTROL) & KEY_SHIFTED) && (wParam == 81))) {
+            DestroyWindow(hWnd);
         }
-        case WM_RBUTTONUP: {
-            process_click(hWnd, lParam, game, 'x');
-            InvalidateRect(hWnd, NULL, TRUE);
-            PostMessage(HWND_BROADCAST, synchMessage, NULL, NULL);
-            return 0;
+        else if ((GetKeyState(VK_SHIFT) & KEY_SHIFTED) && (wParam == 67)) {
+            notepad();
         }
-        case WM_MOUSEWHEEL:
-        {
-            if ((int16_t)HIWORD(wParam) > 0) {
-                game->painter.gridColor += 1;
+
+        switch (wParam) {
+            case VK_RETURN: {
+                game->painter.backgroundColor.set_random();
+                //game->draw();
+                PostMessage(HWND_BROADCAST, synchMessage, NULL, NULL);
+                InvalidateRect(hWnd, NULL, TRUE);
+                return 0;
             }
-            else {
-                game->painter.gridColor -= 1;
-            }
-            //game->draw();
-            PostMessage(HWND_BROADCAST, synchMessage, NULL, NULL);
-            InvalidateRect(hWnd, NULL, TRUE);
-            return 0;
-        }
-        case WM_KEYDOWN: {
-            if (((GetKeyState(VK_CONTROL) & KEY_SHIFTED) && (wParam == 81))) {
+            case VK_ESCAPE: {
                 DestroyWindow(hWnd);
             }
-            else if ((GetKeyState(VK_SHIFT) & KEY_SHIFTED) && (wParam == 67)) {
-                notepad();
-            }
-
-            switch (wParam) {
-                case VK_RETURN: {
-                    game->painter.backgroundColor.set_random();
-                    //game->draw();
-                    PostMessage(HWND_BROADCAST, synchMessage, NULL, NULL);
-                    InvalidateRect(hWnd, NULL, TRUE);
-                    return 0;
-                }
-                case VK_ESCAPE: {
-                    DestroyWindow(hWnd);
-                }
-            }
-
-            return 0;
         }
-        default: {
-            if (msg == synchMessage) {
-                InvalidateRect(hWnd, NULL, TRUE);
-            }
-            break;
 
+        return 0;
+    }
+    else {
+        if (msg == synchMessage) {
+            InvalidateRect(hWnd, NULL, TRUE);
         }
     }
 
